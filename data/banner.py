72e0ed6b3cd2ad2d4152ae7805069129230f614b
mrtripel <75337930+mrtripel@users.noreply.github.com>
2020-12-02 03:59:22 +0700

    Add files via upload

---
+++ data/banner.py
@@ -0,0 +1,84 @@
+import time
+g="\33[0;32m"
+lg="\33[32;1m"
+b="\33[0;36m"
+lb="\33[36;1m"
+r="\33[31;1m"
+w="\33[37;1m"
+b="\33[30;1m"
+y ="\33[33;1m"
+K = "\33[1;33m"
+C = "\033[94;m"
+G = "\033[92;m"
+r = "\033[0;m"
+P = "\033[93;m"
+
+
+
+def bersih():
+    os.system('clear')
+    bersih()
+
+
+def banner():
+    print(f"""{y}
+    +------------------------------------------+
+    |        Welcome To Script Dunia IT        |
+    | Github   : https://github.com/duniait    |
+    | Youtube  : Dunia IT                      |
+    | Facebook : Dunia IT                      |
+    +------------------------------------------+
+            """.format(C,r,G))
+def menu():
+    print(f"{C}[0]{lg} Install Bahan")
+    time.sleep(0.4)
+    print(f"{C}[1]{lg} Hack FB From FriendList")
+    time.sleep(0.4)
+    print(f"{C}[2]{lg} Hack FB From Likes On Post")
+    time.sleep(0.4)
+    print(f"{C}[3]{lg} Hack FB By Search Name")
+    time.sleep(0.4)
+    print(f"{C}[4]{lg} Hack FB From Group")
+    time.sleep(0.4)
+    print(f"{C}[5]{lg} Hack FB From Friend FriendList")
+    time.sleep(0.4)
+    print(f"{C}[6]{lg} Hack WIFI")
+    time.sleep(0.4)
+    print(f"{C}[7]{lg} Lacak Lokasi")
+    time.sleep(0.4)
+    print(f"{C}[8]{lg} Hack CCTV")
+    time.sleep(0.4)
+    print(f"{C}[9]{lg} Lacak Akun Media Sosial")
+    time.sleep(0.4)
+    print(f"{C}[10]{lg} Android Metasploit")
+    time.sleep(0.4)
+    print(f"{C}[11]{lg} Pantesting WEB")
+    time.sleep(0.4)
+    print(f"{C}[12]{lg} OSI FACEBOOK")
+    time.sleep(0.4)
+    print(f"{C}[13]{lg} Phising")
+    time.sleep(0.4)
+    print(f"{C}[14]{lg} Spam SMS")
+    time.sleep(0.4)
+    print(f"{C}[15]{lg} Hack Instagram")
+    time.sleep(0.4)
+    print(f"{C}[16]{lg} Game moonbuggy")
+    time.sleep(0.4)
+    print(f"{C}[17]{lg} Membuat Virus")
+    time.sleep(0.4)
+    print(f"{C}[16]{lg} Peramal Cuaca")
+    time.sleep(0.4)
+    print(f"{C}[17]{lg} Fake ROOT")
+    time.sleep(0.4)
+    print(f"{C}[18]{lg} Download FOTO IG")
+    time.sleep(0.4)
+    print(f"{C}[19]{lg} SPAM CALL")
+    time.sleep(0.4)
+    print(f"{C}[20]{lg} DDOS")
+
+
+
+
+
+ 
+
